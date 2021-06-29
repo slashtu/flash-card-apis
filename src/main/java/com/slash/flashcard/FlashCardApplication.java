3882975ef632f2134514fbacd2d137d284fdad39
slash <y12341234@gmail.com>
2021-06-30 05:16:28 +0900

    spring boot

---
+++ src/main/java/com/slash/flashcard/FlashCardApplication.java
@@ -0,0 +1,13 @@
+package com.slash.flashcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashCardApplication.class, args);
+	}
+
+}
