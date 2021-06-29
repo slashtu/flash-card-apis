5e85a38004f64d52a6bb8e132116109e1710b616
slash <y12341234@gmail.com>
2021-06-30 05:46:28 +0900

    spring updated

--- src/main/java/com/slash/flashcard/FlashCardApplication.java
+++ src/main/java/com/slash/flashcard/FlashCardApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlashCardApplication {
+public class FlashcardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FlashCardApplication.class, args);
+		SpringApplication.run(FlashcardApplication.class, args);
 	}
 
 }
--- src/test/java/com/slash/flashcard/FlashCardApplicationTests.java
+++ src/test/java/com/slash/flashcard/FlashCardApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FlashCardApplicationTests {
+class FlashcardApplicationTests {
 
 	@Test
 	void contextLoads() {
